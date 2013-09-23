b89317d67d055998cae371f281546dba67570270
habales <git@habales.de>
2013-09-23 22:07:43 +0200

    added DB support

--- sniff.py
+++ sniff.py
@@ -1,25 +1,16 @@
 from scapy.all import * 
-from sets import Set
 import time
 import dbutils
 
-#macs = Set()
-
-ds = dbutils.SqliteDataStore('sniffer.db')
-
-
+ds = dbutils.SqliteDataStore('sniffer.db'
 
 def packetHandler(pkt):
 	if pkt.haslayer(Dot11ProbeReq):
-		#write monster log		
-		print '%s\t%s' % (time.time() , pkt.addr2)
-		sys.stdout.flush()
-		#macs.add(pkt.addr2)
+		#print '%s\t%s' % (time.time() , pkt.addr2)
+		#sys.stdout.flush()
+		ds.executeAndCommit('insert into dataline values (?,?)',[time.time(),pkt.addr2])
 
 sniff(iface="mon0", prn=packetHandler, store=0)
 
-#while True:
-#	time.sleep(1)
-#	for mac in macs:
-#		print mac
+
 		
